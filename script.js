1254a34924f9317e453f97055fa47b81a3124f2c
Valguy <valguypmt@gmail.com>
2026-06-21 21:55:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,21 +15,4 @@
         document.getElementById("scroll-progress").style.width = (scrollTop / scrollHeight) * 100 + "%";
         document.getElementById("mini-header").style.display = window.scrollY > 300 ? "block" : "none";
     });
-  function copyIP(e) {
-    const ip = "countries-sullivan.gl.joinmc.link";
-    const tooltip = document.getElementById("cursor-tooltip");
 
-    navigator.clipboard.writeText(ip).then(() => {
-        // Find where the user clicked
-        tooltip.style.left = e.clientX + "px";
-        tooltip.style.top = (e.clientY - 15) + "px";
-        
-        // Show the message
-        tooltip.style.opacity = "1";
-
-        // Hide it after a second
-        setTimeout(() => {
-            tooltip.style.opacity = "0";
-        }, 1000);
-    });
-}
